e2bfcf42203563571fc1bada8455a7729a792e8f
Anna Panova <annapanovadev@gmail.com>
2024-06-06 13:13:47 +0200

    Initial Commit

---
+++ ListOfFriends/ListOfFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfFriendsApp.swift
+//  ListOfFriends
+//
+//  Created by Anna Panova on 06.06.24.
+//
+
+import SwiftUI
+
+@main
+struct ListOfFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
