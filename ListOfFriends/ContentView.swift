79bde46608e53a9d4392edea025204f5260574ee
Anna Panova <annapanovadev@gmail.com>
2024-06-12 09:52:43 +0200

    Set up List with names of friends Add NavigationStack for setting up search bar Add array with filtered friends for searching and reload view

--- ListOfFriends/ContentView.swift
+++ ListOfFriends/ContentView.swift
@@ -9,32 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     @State var name: String = ""
-    @State var listOfFriends: [String] = []
+    @State private var listOfFriends: [String] = ["John","Sam","Samanta","Marcy","Mary"]
+    @State private var search: String = ""
+    
+    @State private var filteredListOfFriends:[String] = []
     
     
     var body: some View {
-        VStack {
-            TextField("name of friend", text:$name )
-                .textFieldStyle(.roundedBorder)
-                .border(.black)
-            
-            // taping finisched, we pusch name into listOfFriends
-                .onSubmit {
-                    listOfFriends.append(name)
-                    name = ""
+        //searchable required NavigationStack in Preview or in body
+        NavigationStack {
+            VStack {
+                TextField("add a friend", text:$name )
+                    .textFieldStyle(.roundedBorder)
+                    .border(.black)
+                
+                //   taping finisched, we pusch name into listOfFriends
+                    .onSubmit {
+                        listOfFriends.append(name)
+                        name = ""
+                        print("name \(name) was append")
+                        filteredListOfFriends = listOfFriends
+                    }
+                
+                // put names from array with names into a List, set up id: \.self to conform protocol Identifiable
+                List(filteredListOfFriends, id: \.self) { name in
+                    Text(name)
                 }
-            Spacer()
-            List(listOfFriends, id: \.self) { name in
-                Text(name)
+                //searchable required NavigationStack!
+                .searchable(text: $search, prompt: "looking for somebody...")
+                .onChange(of: search) {_ in
+                    if search.isEmpty{
+                        filteredListOfFriends = listOfFriends
+                    } else {
+                        filteredListOfFriends = listOfFriends.filter{$0.contains(search)}
+                    }
+                    
+                }
+                
             }
+            .navigationTitle("List of my friends")
+            .padding(10)
+            .onAppear(perform: {
+                filteredListOfFriends = listOfFriends
+                print("Reload Filtered List")
+            })
         }
-       
-        .padding()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
 }
